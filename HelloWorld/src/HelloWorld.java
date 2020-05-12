a58697510d9cb0003b4dee13109c8c15aea008bc
COMPSCISTUDY <COMPSCISTUDY@DESKTOP-M2PCUTN>
2020-05-12 15:30:25 -0700

    Java Tutorials - 2 Hello World

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// Prints the string "Hello World!" in the console
+		System.out.println("Hello World!");
+		System.out.println();
+		System.out.println("This is another string being printed to the console.!!!&&^%241234");
+		System.out.println();
+		System.out.println("This string uses print not println");
+		System.out.println();
+		System.out.println("Last Statement...!");
+
+	}
+
+}
