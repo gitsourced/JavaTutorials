7d7f9e219f267eb2dca843a7aaec91710ce3a556
COMPSCISTUDY <COMPSCISTUDY@DESKTOP-M2PCUTN>
2020-05-12 15:36:36 -0700

    Java Tutorials - 3 String Concatenation

---
+++ StringConcat/src/stringConcat.java
@@ -0,0 +1,13 @@
+
+public class stringConcat {
+
+	public static void main(String[] args) {
+
+		System.out.println("Label: " + "product");
+		System.out.println(2+2);
+		System.out.println("Price: " + 2 + 2);
+		System.out.println(2 + 2 + "Price");
+		
+	}
+
+}
