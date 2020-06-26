b68b935f5b43527e54eacc284b07883c40e68404
COMPSCISTUDY <COMPSCISTUDY@DESKTOP-M2PCUTN>
2020-06-26 15:18:54 -0700

    Java Tutorials - 7 Introduction to Objects and Classes

---
+++ ObjectsClasses/src/ObjectClassesExample.java
@@ -0,0 +1,13 @@
+public class ObjectClassesExample {
+
+	public static void main(String[] args) {
+		
+		String name = new String("Bob");
+		
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase());
+		
+		System.out.println(name.equalsIgnoreCase("bob"));
+		System.out.println(name.charAt(1));
+	}
+}
