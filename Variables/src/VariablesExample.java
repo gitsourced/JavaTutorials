9b6466c2f74ada5b1c4dfcfd31b806dfd7b8043f
COMPSCISTUDY <COMPSCISTUDY@DESKTOP-M2PCUTN>
2020-06-26 13:55:59 -0700

    Java Tutorials - 4 Variables

---
+++ Variables/src/VariablesExample.java
@@ -0,0 +1,28 @@
+public class VariablesExample {
+
+	public static void main(String[] args) {
+		
+		// Integer Example
+		int firstInt = 14;
+		System.out.println(firstInt);
+
+		// Float Example
+		int firstFloat = 14;
+		System.out.println(firstFloat / 3);
+
+		// Double Example
+		int firstDouble = 14;
+		System.out.println(firstDouble / 3);
+
+		// Boolean Example
+		boolean check = true;
+		System.out.println(check);
+		System.out.println(!check);
+
+		// Char Example
+		char firstChar = '&';
+		System.out.println(firstChar);
+		int charValue = firstChar;
+		System.out.println("The intgeger value for " + firstChar + " is " + charValue);
+	}
+}
